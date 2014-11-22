d55a30f3da715c53f0fd976db47cf3d248ffe65b
Matthew Kam <matt.ph.kam@gmail.com>
2014-11-22 12:04:17 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weebook::Application.config.secret_key_base = '548e3d986a06d82c0701ec755688a9ae885c4148994a690b9004eef25e598b37973226937324953a8c397354c078eece520554d3a88ca1bcb8ee6f4312bfae6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Weebook::Application.config.session_store :cookie_store, key: '_weebook_session'
